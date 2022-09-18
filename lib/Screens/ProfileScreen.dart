09100bac8b5c9ae12e51a1169bdaff719dc3d056
mardodmr <mardo.dmr@gmail.com>
2022-09-19 02:15:24 +0300

    get data

--- lib/Screens/ProfileScreen.dart
+++ lib/Screens/ProfileScreen.dart
@@ -1,9 +1,13 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors, sized_box_for_whitespace, avoid_unnecessary_containers, use_key_in_widget_constructors
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:country_code_picker/country_code_picker.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:regexed_validator/regexed_validator.dart';
 import '../Utils/ThemeConstants.dart';
+import '../Utils/constant.dart';
 import 'HomeScreen.dart';
 import 'TextFieldWidget.dart';
 
@@ -12,12 +16,30 @@ class ProfileScreen extends StatefulWidget {
 
   @override
   State<ProfileScreen> createState() => _ProfileScreenState();
-}
 
+}
+enum SingingCharacter { male, female }
 class _ProfileScreenState extends State<ProfileScreen> {
-  final TextEditingController _phoneController =
-      TextEditingController(text: '508400700');
+
   String countryCode = "+1";
+  final formKey= GlobalKey<FormState>();
+  var db = FirebaseFirestore.instance.collection('users');
+  String myemail = "";
+  String myphone = "";
+  String myaddress = "";
+
+  TextEditingController _address = TextEditingController();
+  TextEditingController _email = TextEditingController();
+  final TextEditingController _phoneController = TextEditingController();
+
+  _fetchData() async{
+    final firebaseUser = FirebaseAuth.instance.currentUser!;
+    await FirebaseFirestore.instance.collection('users')
+        .doc(firebaseUser.uid).get().then((value) => {myemail: 'email', myphone: 'phone', myaddress: 'address'});
+  }
+
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -110,167 +132,268 @@ class _ProfileScreenState extends State<ProfileScreen> {
                 ),
               ),
             ),
-            Container(
-              padding: EdgeInsets.all(10.0),
-              width: double.infinity,
-              decoration: BoxDecoration(
-                color: Theme.of(context).primaryColor,
-                borderRadius: BorderRadius.circular(8),
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Text(
-                    "Phone Number",
-                    //textAlign: TextAlign.start,
-                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(
-                    height: 8,
-                  ),
-                  // Padding(
-                  //   padding: EdgeInsets.symmetric(vertical: 20.0),
-                  //   child: Container(
-                  //     padding: EdgeInsets.symmetric(vertical: 10.0),
-                  //     width: double.infinity,
-                  //     decoration: BoxDecoration(
-                  //       color: Color(0xF5F5F5FF),
-                  //       borderRadius: BorderRadius.circular(8),
-                  //     ),
-                  //     child: Text(""),
-                  //   ),
-                  // ),
-                  Container(
-                    width: double.infinity,
-                    height: 45,
-                    padding: EdgeInsets.only(left: 20),
-                    decoration: BoxDecoration(
-                      color: Theme.of(context).accentColor,
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          "00 971 50 840 0700",
-                          textAlign: TextAlign.start,
-                          style: TextStyle(
-                            fontSize: 14,
-                            fontWeight: FontWeight.w600,
-                          ),
+            SingleChildScrollView(
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                width: double.infinity,
+                color: Colors.white,
+                child: Form(
+                  key: formKey,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      SizedBox(
+                        height: 8,
+                      ),
+                      Text(
+                        "Address",
+                        style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(
+                        height: 8,
+                      ),
+                      TextFormField(
+                        keyboardType: TextInputType.text,
+                        validator: (value){
+                          if(value!.isEmpty){
+                            return "Enter a valid address";
+                          }
+                        },
+                        controller: _address,
+                        onChanged: (val) {},
+                        decoration: kTextFieldDecoration.copyWith(
+                          hintText: myaddress,
                         ),
-                      ],
-                    ),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Text(
-                    "Email",
-                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(
-                    height: 8,
-                  ),
-                  Container(
-                    width: double.infinity,
-                    height: 45,
-                    padding: EdgeInsets.only(left: 20),
-                    decoration: BoxDecoration(
-                      color: Theme.of(context).accentColor,
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          "ahmadjalal@gmail.com",
-                          textAlign: TextAlign.start,
-                          style: TextStyle(
-                            fontSize: 14,
-                            fontWeight: FontWeight.w600,
-                          ),
+                      ),
+                      SizedBox(
+                        height: 8,
+                      ),
+
+                      SizedBox(
+                        height: 8,
+                      ),
+                      Text(
+                        "Phone Number",
+                        style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(
+                        height: 8,
+                      ),
+                      TextFormField(
+                        keyboardType: TextInputType.text,
+                        validator: (value){
+                          if(value!.isEmpty){
+                            return "Enter a valid phone number";
+                          }
+                        },
+                        controller: _phoneController,
+                        onChanged: (val) {},
+                        decoration: kTextFieldDecoration.copyWith(
+                          hintText: "+963 933 111 222",
                         ),
-                      ],
-                    ),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Text(
-                    "Gender",
-                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(
-                    height: 8,
-                  ),
-                  Container(
-                    width: double.infinity,
-                    height: 45,
-                    padding: EdgeInsets.only(left: 20),
-                    decoration: BoxDecoration(
-                      color: Theme.of(context).accentColor,
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          "Male",
-                          textAlign: TextAlign.start,
-                          style: TextStyle(
-                            fontSize: 14,
-                            fontWeight: FontWeight.w600,
-                          ),
+                      ),
+                      SizedBox(
+                        height: 8,
+                      ),
+                      Text(
+                        "Email",
+                        style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(
+                        height: 8,
+                      ),
+                      TextFormField(
+                        keyboardType: TextInputType.emailAddress,
+                        validator: (value){
+                          if(!validator.email(value!)){
+                            return "Please enter a valid email";
+                          }
+                        },
+                        onChanged: (val) {},
+                        controller: _email,
+                        decoration: kTextFieldDecoration.copyWith(
+                          hintText: 'Email',
                         ),
-                      ],
-                    ),
+                      ),
+                      SizedBox(
+                        height: 8,
+                      ),
+
+                      SizedBox(
+                        height: 30,
+                      ),
+                      SizedBox(
+                        height: 8,
+                      ),
+                    ],
                   ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Text(
-                    "Date of Birth",
-                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(
-                    height: 8,
-                  ),
-                  Container(
-                    width: double.infinity,
-                    height: 45,
-                    padding: EdgeInsets.only(left: 20),
-                    decoration: BoxDecoration(
-                      color: Theme.of(context).accentColor,
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          "Not Set",
-                          textAlign: TextAlign.start,
-                          style: TextStyle(
-                            fontSize: 14,
-                            fontWeight: FontWeight.w600,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                ],
+                ),
               ),
             ),
+            // Container(
+            //   padding: EdgeInsets.all(10.0),
+            //   width: double.infinity,
+            //   decoration: BoxDecoration(
+            //     color: Theme.of(context).primaryColor,
+            //     borderRadius: BorderRadius.circular(8),
+            //   ),
+            //   child: Column(
+            //     crossAxisAlignment: CrossAxisAlignment.stretch,
+            //     children: [
+            //       Text(
+            //         "Phone Number",
+            //         //textAlign: TextAlign.start,
+            //         style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+            //       ),
+            //       SizedBox(
+            //         height: 8,
+            //       ),
+            //       // Padding(
+            //       //   padding: EdgeInsets.symmetric(vertical: 20.0),
+            //       //   child: Container(
+            //       //     padding: EdgeInsets.symmetric(vertical: 10.0),
+            //       //     width: double.infinity,
+            //       //     decoration: BoxDecoration(
+            //       //       color: Color(0xF5F5F5FF),
+            //       //       borderRadius: BorderRadius.circular(8),
+            //       //     ),
+            //       //     child: Text(""),
+            //       //   ),
+            //       // ),
+            //       Container(
+            //         width: double.infinity,
+            //         height: 45,
+            //         padding: EdgeInsets.only(left: 20),
+            //         decoration: BoxDecoration(
+            //           color: Theme.of(context).accentColor,
+            //           borderRadius: BorderRadius.circular(8),
+            //         ),
+            //         child: Column(
+            //           mainAxisAlignment: MainAxisAlignment.center,
+            //           crossAxisAlignment: CrossAxisAlignment.start,
+            //           children: [
+            //             Text(
+            //               "00 971 50 840 0700",
+            //               textAlign: TextAlign.start,
+            //               style: TextStyle(
+            //                 fontSize: 14,
+            //                 fontWeight: FontWeight.w600,
+            //               ),
+            //             ),
+            //           ],
+            //         ),
+            //       ),
+            //       SizedBox(
+            //         height: 20,
+            //       ),
+            //       Text(
+            //         "Email",
+            //         style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+            //       ),
+            //       SizedBox(
+            //         height: 8,
+            //       ),
+            //       Container(
+            //         width: double.infinity,
+            //         height: 45,
+            //         padding: EdgeInsets.only(left: 20),
+            //         decoration: BoxDecoration(
+            //           color: Theme.of(context).accentColor,
+            //           borderRadius: BorderRadius.circular(8),
+            //         ),
+            //         child: Column(
+            //           mainAxisAlignment: MainAxisAlignment.center,
+            //           crossAxisAlignment: CrossAxisAlignment.start,
+            //           children: [
+            //             Text(
+            //               "ahmadjalal@gmail.com",
+            //               textAlign: TextAlign.start,
+            //               style: TextStyle(
+            //                 fontSize: 14,
+            //                 fontWeight: FontWeight.w600,
+            //               ),
+            //             ),
+            //           ],
+            //         ),
+            //       ),
+            //       SizedBox(
+            //         height: 20,
+            //       ),
+            //       Text(
+            //         "Gender",
+            //         style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+            //       ),
+            //       SizedBox(
+            //         height: 8,
+            //       ),
+            //       Container(
+            //         width: double.infinity,
+            //         height: 45,
+            //         padding: EdgeInsets.only(left: 20),
+            //         decoration: BoxDecoration(
+            //           color: Theme.of(context).accentColor,
+            //           borderRadius: BorderRadius.circular(8),
+            //         ),
+            //         child: Column(
+            //           mainAxisAlignment: MainAxisAlignment.center,
+            //           crossAxisAlignment: CrossAxisAlignment.start,
+            //           children: [
+            //             Text(
+            //               "Male",
+            //               textAlign: TextAlign.start,
+            //               style: TextStyle(
+            //                 fontSize: 14,
+            //                 fontWeight: FontWeight.w600,
+            //               ),
+            //             ),
+            //           ],
+            //         ),
+            //       ),
+            //       SizedBox(
+            //         height: 20,
+            //       ),
+            //       Text(
+            //         "Date of Birth",
+            //         style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+            //       ),
+            //       SizedBox(
+            //         height: 8,
+            //       ),
+            //       Container(
+            //         width: double.infinity,
+            //         height: 45,
+            //         padding: EdgeInsets.only(left: 20),
+            //         decoration: BoxDecoration(
+            //           color: Theme.of(context).accentColor,
+            //           borderRadius: BorderRadius.circular(8),
+            //         ),
+            //         child: Column(
+            //           mainAxisAlignment: MainAxisAlignment.center,
+            //           crossAxisAlignment: CrossAxisAlignment.start,
+            //           children: [
+            //             Text(
+            //               "Not Set",
+            //               textAlign: TextAlign.start,
+            //               style: TextStyle(
+            //                 fontSize: 14,
+            //                 fontWeight: FontWeight.w600,
+            //               ),
+            //             ),
+            //           ],
+            //         ),
+            //       ),
+            //       SizedBox(
+            //         height: 10,
+            //       ),
+            //     ],
+            //   ),
+            // ),
           ],
         ),
       ),
     );
+
   }
+
 }
